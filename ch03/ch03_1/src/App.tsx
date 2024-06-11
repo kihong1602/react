a3aa131309a2294414b7fc80e106a36fca338f64
Kihong <kks4517@naver.com>
2024-06-11 18:13:31 +0900

    ch03_01

---
+++ ch03/ch03_1/src/App.tsx
@@ -0,0 +1,17 @@
+import UsingIconWithCSSClass from './pages/UsingIconWithCSSClass.tsx'
+import UsingIcon from './pages/UsingIcon.tsx'
+import Style from './pages/Style.tsx'
+import Icon from './pages/Icon.tsx'
+import Bootstrap from './pages/Bootstrap.tsx'
+
+export default function App() {
+  return (
+    <div>
+      <UsingIconWithCSSClass />
+      <UsingIcon />
+      <Style />
+      <Icon />
+      <Bootstrap />
+    </div>
+  )
+}
---
+++ ch03/ch03_1/src/data/chance.ts
@@ -0,0 +1,13 @@
+import Chance from 'chance'
+
+const chance = new Chance()
+
+export const randomUUID = () => chance.guid()
+export const randomName = () => chance.name()
+export const randomEmail = () => chance.email()
+export const randomId = () => chance.fbid()
+export const randomJobTitle = () => chance.profession()
+export const randomCompanyName = () => chance.company()
+export const randomSentence = (words: number = 5): string => chance.sentence({words})
+export const randomTitleText = (words: number = 3): string => chance.sentence({words})
+export const randomParagraphs = (sentences = 3): string => chance.paragraph({sentences})
