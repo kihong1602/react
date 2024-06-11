94575987ccfa79991d90c5916bdf47bc62e5c2fc
Kihong <kks4517@naver.com>
2024-06-11 21:25:30 +0900

    ch03_02

---
+++ ch03/ch03_2/src/App.tsx
@@ -0,0 +1,11 @@
+import TextsTest from './pages/TextsTest.tsx'
+
+export default function App() {
+  return (
+    <div>
+      {/*<Color />*/}
+      {/*<Tailwindcss />*/}
+      <TextsTest />
+    </div>
+  )
+}
