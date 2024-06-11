ba0168101f92ab57b488919aaa6dfcb31cef1943
Kihong <kks4517@naver.com>
2024-06-11 12:15:06 +0900

    ch02_03 2

--- ch02/ch02_3/src/App.tsx
+++ ch02/ch02_3/src/App.tsx
@@ -1,13 +1,12 @@
-import {Component} from 'react'
 import ClassComponent from './ClassComponent.tsx'
+import ArrowComponent from './ArrowComponent.tsx'
 
-export default class App extends Component {
-  render() {
-    return (
-      <ul>
-        <ClassComponent href={'https://google.com'} text={'go to Google'} />
-        <ClassComponent href={'https://twitter.com'} text={'go to Twitter'} />
-      </ul>
-    )
-  }
+
+export default function App() {
+  return (
+    <ul>
+      <ClassComponent href={'https://google.com'} text={'go to Google'} />
+      <ArrowComponent href={'https://twitter.com'} text={'go to Twitter'} />
+    </ul>
+  )
 }
