74f0464c923d128305c0829b7c626e137f8f949a
Kihong <kks4517@naver.com>
2024-06-12 10:33:34 +0900

    ch03_03 2

--- ch03/ch03_3/src/components/Div.tsx
+++ ch03/ch03_3/src/components/Div.tsx
@@ -1,16 +1,19 @@
 import {DetailedHTMLProps, FC, HTMLAttributes, PropsWithChildren} from "react";
 import {WidthHeight} from "./WidthHeight.ts";
+import {LeftRightTopBottom} from "./LeftRightTopBottom.ts";
 
 
 export type ReactDivProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
 
-export type Divprops = ReactDivProps & PropsWithChildren<WidthHeight> & {
+export type Divprops = ReactDivProps &
+    PropsWithChildren<WidthHeight> &
+    LeftRightTopBottom & {
   src?: string
 }
 
 export const Div: FC<Divprops> = (
-    {width, height, style: _style, src, className: _className, ...props}) => {
-  const style = {..._style, width, height, backgroundImage: src && `url(${src})`}
+    {width, height, style: _style, src, className: _className, left, right, top, bottom, ...props}) => {
+  const style = {..._style, width, height, backgroundImage: src && `url(${src})`, left, right, top, bottom}
   const className = ['box-sizing', src && 'bg-gray-300', _className].join(' ')
   return <div {...props} className={className} style={style}/>
 }
--- ch03/ch03_3/src/pages/DisplayNoneTest.tsx
+++ ch03/ch03_3/src/pages/DisplayNoneTest.tsx
@@ -1,10 +1,14 @@
 import {Title} from "../components";
 
-export default function DisplayNoneTest(){
+export default function DisplayNoneTest() {
   return (
       <section className={"mt-4"}>
         <Title>DisplayNoneTest</Title>
-        <div className={"mt-4"}></div>
+        <div className={"mt-4"}>
+          <p className={"visible"}>visibility: visible text</p>
+          <p className={"invisible"}>visibility: hidden text</p>
+          <p className={"hidden"}>display: none text</p>
+        </div>
       </section>
   )
 }
