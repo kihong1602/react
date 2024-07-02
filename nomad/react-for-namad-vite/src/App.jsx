cfe590ab5270d9058eae7213d69f6ea114118011
Kihong <kks4517@naver.com>
2024-07-02 14:53:37 +0900

    nomad#6.1 useEffect

--- nomad/react-for-namad-vite/src/App.jsx
+++ nomad/react-for-namad-vite/src/App.jsx
@@ -1,9 +1,12 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 
 function App() {
   const [counter, setCounter] = useState(0);
   const onClick = () => setCounter((prev) => prev + 1);
-  console.log('call api');
+  console.log('I run all time');
+  useEffect(() => {
+    console.log('Call api');
+  }, []); // 최초 렌더링 시에만 실행할 작업을 할당할 때 useEffect 훅을 사용함.
   return (
       <div>
         <h1>{counter}</h1>
