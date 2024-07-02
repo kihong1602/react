b3b9f801ad7218753f1995f46695ecce107c56c8
Kihong <kks4517@naver.com>
2024-07-02 15:47:10 +0900

    nomad#6.2 Deps

--- nomad/react-for-namad-vite/src/App.jsx
+++ nomad/react-for-namad-vite/src/App.jsx
@@ -2,13 +2,24 @@ import {useEffect, useState} from 'react'
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("")
   const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+
   console.log('I run all time');
   useEffect(() => {
     console.log('Call api');
   }, []); // 최초 렌더링 시에만 실행할 작업을 할당할 때 useEffect 훅을 사용함.
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log(`Search for ${keyword}`)
+    }
+  }, [keyword]) // keyword 변수가 변경될 때만 실행됨
+
   return (
       <div>
+        <input type="text" placeholder={'Search Here'} value={keyword} onChange={onChange}/>
         <h1>{counter}</h1>
         <button onClick={onClick}>Click Me</button>
       </div>
