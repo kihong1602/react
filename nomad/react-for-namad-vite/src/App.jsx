58e9cc6039c708d0645640ce7ba8f0539850f014
Kihong <kks4517@naver.com>
2024-07-02 16:44:52 +0900

    nomad#6.3 Recap

--- nomad/react-for-namad-vite/src/App.jsx
+++ nomad/react-for-namad-vite/src/App.jsx
@@ -2,20 +2,21 @@ import {useEffect, useState} from 'react'
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("")
-  const onClick = () => setCounter((prev) => prev + 1);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter(prev => prev + 1);
   const onChange = (e) => setKeyword(e.target.value);
 
-  console.log('I run all time');
+  console.log('I Running All Times')
+
   useEffect(() => {
-    console.log('Call api');
-  }, []); // 최초 렌더링 시에만 실행할 작업을 할당할 때 useEffect 훅을 사용함.
+    console.log('Call API');
+  }, []);
 
   useEffect(() => {
     if (keyword !== "" && keyword.length > 5) {
-      console.log(`Search for ${keyword}`)
+      console.log(`Search For ${keyword}`)
     }
-  }, [keyword]) // keyword 변수가 변경될 때만 실행됨
+  }, [keyword]);
 
   return (
       <div>
