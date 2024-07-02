d743a4ea8799bb901611e6d58d6cfcf9ba4ba495
Kihong <kks4517@naver.com>
2024-07-02 19:00:19 +0900

    nomad#7.0 To Do List part_1

--- nomad/react-for-namad-vite/src/App.jsx
+++ nomad/react-for-namad-vite/src/App.jsx
@@ -1,23 +1,32 @@
-import {useEffect, useState} from 'react'
-
-const Hello = () => {
-  useEffect(() => {
-    console.log("Created")
-    return () => console.log("Destroyed")
-  }, []);
-
-  return <h1>Hello</h1>
-}
+import {useEffect, useState} from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing(prev => !prev);
-  return (
-      <div>
-        {showing ? <Hello/> : null}
-        <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-      </div>
-  )
+  const [todo, setTodo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setTodo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodo("");
+    setToDos(prev => [...prev, todo]);
+  }
+  useEffect(() => {
+    console.log(toDos);
+  }, [toDos]);
+
+  return <div>
+    <h1>My To Dos ({toDos.length})</h1>
+    <form onSubmit={onSubmit}>
+      <input type="text"
+             placeholder={'Write your to do...'}
+             value={todo}
+             onChange={onChange}
+      />
+      <button>Add To Do</button>
+    </form>
+  </div>;
 }
 
 export default App;
